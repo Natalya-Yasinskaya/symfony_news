0d486e6383cf0c68b65130fd16b2b578c9b06f1a
Natalya Yasinskaya <Natalya.ponomarenko.94@mail.ru>
2022-08-07 22:55:45 +0300

    add cors policy rules

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header("Allow: *");
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === "OPTIONS") {
+    die();
+}
 
 use App\Kernel;
 
